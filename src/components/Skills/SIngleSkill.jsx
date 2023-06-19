d560efce42a043b0e4826e5d57bc813ba468f35e
masud001 <madhnagar@gmail.com>
2023-06-20 01:40:46 +0600

    single skill component added to develop

---
+++ src/components/Skills/SIngleSkill.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SIngleSkill = () => {
+    return (
+        <li className={`skills__list-item`}>
+            <div className="skill__title">
+                <h6>title</h6>
+            </div>
+            <div className="skills__lists">
+                <span>javaScript, html, css, js</span>
+            </div>
+        </li>
+    )
+}
+
+export default SIngleSkill
